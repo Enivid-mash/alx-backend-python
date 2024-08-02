11b05643e759a3cdea9fd76b73f3527df41254d0
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-08-02 04:30:02 +0100

    Add first directory

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A module containing a function to add two float numbers.
+
+This module defines a type-annotated function `add` that takes two
+floating-point numbers as arguments and returns their sum.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
